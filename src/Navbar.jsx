9cf0f776a6e67cca04637fe7258c4094ca2bfdf8
ziyangchua02 <czy3k1@gmail.com>
2025-09-22 16:24:08 +0800

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ onNavigate, currentPage }) => {
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-brand">
-          <img src="/img/logo.png" alt="Paxed Logo" className="navbar-logo" />
+          <a className="navbar-libk">PAXED</a>
         </div>
         <ul className="navbar-menu">
           <li><a href="#home" className="navbar-link" onClick={(e) => handleNavClick('home', e)}>Home</a></li>
